b80cfe34d47bdaaa7f01de0c62f8db3a81a0aa2b
m-ana <nkichimonji@gmail.com>
2021-08-16 16:17:53 +0700

    first commit

---
+++ database/migrations/2021_08_16_000005_create_data_perangkat_keras_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDataPerangkatKerasTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('data_perangkat_keras', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('tipe')->nullable();
+            $table->string('serial_number')->nullable();
+            $table->integer('tahun_beli')->nullable();
+            $table->string('nomor_u')->nullable();
+            $table->longText('keterangan')->nullable();
+            $table->string('ip')->nullable();
+            $table->date('tahun_berakhir_garansi')->nullable();
+            $table->string('nomor_u_kosong')->nullable();
+            $table->string('kontak_pic')->nullable();
+            $table->string('telepon')->nullable();
+            $table->string('perusahaan')->nullable();
+            $table->string('pengampu')->nullable();
+            $table->string('ruang_panel')->nullable();
+            $table->timestamps();
+        });
+    }
+}
