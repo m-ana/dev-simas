bd20025ad01a4b6514823e8cc5d091658b6247c3
m-ana <nkichimonji@gmail.com>
2021-08-21 09:51:21 +0700

    change app name

--- resources/lang/id/panel.php
+++ resources/lang/id/panel.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'site_title' => 'Sistem Manajemen Aset Teknologi Informasi',
-    'side_title' => 'Si Tomas'
+    'site_title' => 'Sistem Manajemen Aset Perangkat Teknologi Informasi',
+    'side_title' => 'SIMARTI'
 ];
